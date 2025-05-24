eb6d6a583f7695d6d67c04d90c17b1faa34e087e
nukestye <nuke.stye@gmail.com>
2025-05-24 22:01:27 +0100

    feat: homepage introduction

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,34 @@
 
-// Components
-// import Page from './Components/Page.js';
-
 // CSS
 import './Components/css/main.css';
+import './Components/css/home.css';
+
+import logo from './assests/logo3.png';
 
 function Home() {
   return (
     <div className="container">
-      
+      <div className='intro'>
+      <div className='intro-text'>
+        <p className='intro-text-start'>
+          Hello, <br />
+          I am <span className='plum'>gohar</span>
+        </p>
+
+        <p className='intro-text-mid'>
+          aka <span className='plum'>nukestye</span>
+        </p>
+
+        <p className='intro-text-end'>
+          a Software Engineer/Robotic enthusiast
+        </p>
+      </div>
+
+      <img src={logo} height={256} width={256} alt='cat shaped letter g'/>
+
+      </div>
+      <div></div>
+      <div></div>
     </div>
   );
 }
