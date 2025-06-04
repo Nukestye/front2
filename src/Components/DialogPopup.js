e9d78c3a2b61c7a12b69ac84267962e2c1a69a92
nukestye <nuke.stye@gmail.com>
2025-06-04 14:34:30 +0100

    feat: updated simple dialog

--- src/Components/DialogPopup.js
+++ src/Components/DialogPopup.js
@@ -1,3 +1,5 @@
+import React, { Fragment } from 'react';
+import { createPortal } from 'react-dom';
 import './css/popup.css';
 
 export function DialogPopup({
@@ -11,36 +13,24 @@ export function DialogPopup({
         option2Value=1 
     }) {
 
-    var style = {
-        visibility: 'hidden'
-    };
-
-    if (open) {
-        style = {
-            visibility: 'visible'
-        };
-    } else {
-        style = {
-            visibility: 'hidden',
-        };
-    }
-
-    return (
-        <div className="popup" style={style}>
-            <div className='popup-content' style={style}>
-                <span className='popup-message'>{message}</span>
-                <br />
-                <div className="popup-options">
-                    <button onClick={() => {
-                        setOutput(option1Value);
-                        setOpen(false);
-                        }}> {option1} </button>
-                    <button onClick={() => {
-                        setOutput(option2Value);
-                        setOpen(false);
-                        }}> {option2} </button>
+    return open ? createPortal(
+        <Fragment>
+                <div className="popup">
+                    <div className='popup-content'>
+                        <span className='popup-message'>{message}</span>
+                        <br />
+                        <div className="popup-options">
+                            <button onClick={() => {
+                                setOutput(option1Value);
+                                setOpen(false);
+                                }}> {option1} </button>
+                            <button onClick={() => {
+                                setOutput(option2Value);
+                                setOpen(false);
+                                }}> {option2} </button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    )
+        </Fragment>, document.body
+    ) : null
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -31,7 +31,6 @@ function Home() {
       <div>
         <br />
       </div>
-      <div></div>
     </div>
   );
 }
